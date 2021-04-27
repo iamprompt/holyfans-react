c76f553561ed2761fd928d87d430ff0659245311
Supakarn Laorattanakul <wearedprompt@gmail.com>
2021-04-27 13:03:00 +0700

    Integrate with TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  build: {
+    emptyOutDir: true,
+  },
+  plugins: [reactRefresh()],
 })
